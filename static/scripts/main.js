111f752e9e23b1e15f3bf462aabb1b91fe07a2f5
lieutandat <lieutandat@gmail.com>
2018-05-09 22:10:04 +0700

    fdsfsdf

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -15,7 +15,7 @@
 // Register Service Worker
 
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+  navigator.serviceWorker.register('service-worker.js').then(function(registration) {
     // Registration was successful
   }).catch(function(err) {
     // registration failed :(
