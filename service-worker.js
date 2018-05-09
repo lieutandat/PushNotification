83acfcd488c1dc4051407e7ef1180536f114920d
lieutandat <lieutandat@gmail.com>
2018-05-09 22:26:52 +0700

    dsdas

--- service-worker.js
+++ service-worker.js
@@ -19,3 +19,17 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   console.log('Service Worker activating.');  
 });
+
+self.addEventListener('push', function(event) {  
+  var title = 'Yay a message.';  
+  var body = 'We have received a push message.';  
+  var icon = '/images/smiley.svg';  
+  var tag = 'simple-push-example-tag';
+  event.waitUntil(  
+    self.registration.showNotification(title, {  
+      body: body,  
+      icon: icon,  
+      tag: tag  
+    })  
+  );  
+});
